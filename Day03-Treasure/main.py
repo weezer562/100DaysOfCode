364bb7441124143dbf44befbbedc94ce5dcaa0a7
weezer562 <weezer562@gmail.com>
2024-03-03 14:12:50 -0800

    Day 3 Project

---
+++ Day03-Treasure/main.py
@@ -0,0 +1,48 @@
+treasure = ''''*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____/_
+*******************************************************************************'''''
+
+print(treasure)
+
+while True:
+    print("Welcome to Treasure Island.")
+    print("Your mission is to find the treasure.")
+
+    # Path to win the game: left, wait, yellow
+    # Path to lose the game: right, swim, red, blue, or any other input
+    choiceDirection = input('You\'re at a crossroad, where do you want to go? Type "left" or "right"\n').lower()
+
+    if choiceDirection == "left":
+        choiceSwimOrWait = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
+        if choiceSwimOrWait == "wait":
+            choiceDoor = input('You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n').lower()
+            if choiceDoor == "red":
+                print("It's a room full of fire. Game Over.\n")
+            elif choiceDoor == "yellow":
+                print("You found the treasure! You Win!\n")
+            elif choiceDoor == "blue":
+                print("You enter a room of beasts. Game Over.\n")
+            else:
+                print("You chose a door that doesn't exist. Game Over.\n")
+        else:
+            print("You get attacked by an angry trout. Game Over.\n")
+    else:
+        print("You fell into a hole. Game Over.\n")
+
