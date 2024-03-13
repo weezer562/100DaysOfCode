57608841eb2d65c77c430145487bafd7b92d7190
weezer562 <weezer562@gmail.com>
2024-03-13 00:42:15 -0700

    Day 21 Snake Game complete

--- Day20-21-SnakeGame/main.py
+++ Day20-21-SnakeGame/main.py
@@ -1,15 +1,24 @@
 import time
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
+SCREEN_WIDTH = 600
+SCREEN_LENGTH = 600
+
+# Screen Setup
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_LENGTH)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-       
+food = Food()
+scoreboard = Scoreboard()
+
+# Control Setup
 screen.listen()
 screen.onkey(key="Left", fun=snake.turn_left)
 screen.onkey(key="Right", fun=snake.turn_right)
@@ -17,33 +26,26 @@ screen.onkey(key="Up", fun=snake.turn_up)
 screen.onkey(key="Down", fun=snake.turn_down)
 
 game_is_on = True
+
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     
-    snake.mov()
+    snake.move()
     
+    # Detect Collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.update_score()
+        snake.grow()
+        
+    # Border detection collision
+    game_is_on = snake.border_check(SCREEN_WIDTH, SCREEN_LENGTH)
     
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # Detect collision with self tail
+    for segment in snake.snake[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+            
 screen.exitonclick()
--- Day20-21-SnakeGame/snake.py
+++ Day20-21-SnakeGame/snake.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+from scoreboard import Scoreboard
 
 MOVE_DISTANCE = 20
 UP = 90
@@ -7,24 +8,20 @@ LEFT = 180
 RIGHT = 0
 
 class Snake:
-   
     def __init__(self):
         self.snake = []
         self.make_snake()
         self.head = self.snake[0]
     
     def make_snake(self):
+        """Create starting Snake"""
         x_coord = 0.0
         y_coord = 0.0
         for _ in range(0,3):
-            new_square = Turtle("square")
-            new_square.penup()
-            new_square.color("white")
-            new_square.setposition(x_coord, y_coord)
-            x_coord -= MOVE_DISTANCE   
-            self.snake.append(new_square)
-            
-    def mov(self):
+            self.add_segment(x_coord, y_coord)
+           
+    def move(self):
+        """Move Snake segments as one"""
         for snake_num in range(len(self.snake) - 1, 0, -1):
             new_x = self.snake[snake_num - 1].xcor()
             new_y = self.snake[snake_num - 1].ycor()
@@ -33,6 +30,37 @@ class Snake:
         
         self.head.forward(MOVE_DISTANCE)
 
+    def border_check(self, x_coord, y_coord):
+        """Check if snake reached a screen edge"""
+        x = x_coord/2 - 20
+        y = y_coord/2
+        
+        scoreboard = Scoreboard()
+        if self.head.xcor() > x or self.head.xcor() <= -y:
+            scoreboard.game_over()
+            return False
+            
+        if self.head.ycor() > x or self.head.ycor() < -y:
+            scoreboard.game_over()
+            return False
+        return True
+        
+    def add_segment(self, x_coord, y_coord):
+        """Add segment to Snake"""
+        x_coord += MOVE_DISTANCE
+        new_square = Turtle("square")
+        new_square.penup()
+        new_square.color("white")
+        new_square.setposition(x_coord, y_coord)
+        self.snake.append(new_square)
+        
+    def grow(self):
+        """Grow the snake"""
+        x = self.snake[-1].xcor()
+        y = self.snake[-1].ycor()
+        
+        self.add_segment(x, y)
+        
     def turn_left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
