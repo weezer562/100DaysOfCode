b73fd6234644b6f7b564cbfdf5d8ed2d964083c3
weezer562 <weezer562@gmail.com>
2024-03-14 01:11:18 -0700

    Removes default arrow in Car manager

--- Day23-TurtleCrossing/car_manager.py
+++ Day23-TurtleCrossing/car_manager.py
@@ -16,6 +16,7 @@ class CarManager(Turtle):
 
         self.all_cars = []
         self.car_speed = STARTING_MOVE_DISTANCE
+        self.hideturtle()
 
     def create_car(self):
         random_chance = random.randint(1,6)
