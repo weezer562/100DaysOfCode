e7fe16fb99aaf15f47db4de84243a4b847087441
weezer562 <weezer562@gmail.com>
2024-03-18 16:47:06 -0700

    Day 32 - Quote of the Day Email

---
+++ Day32-QuoteOfTheDay/main.py
@@ -0,0 +1,37 @@
+import smtplib
+import datetime as dt
+import random
+import os
+from dotenv import load_dotenv
+
+load_dotenv('../creds.env')
+
+
+def get_quote():
+    with open("quotes.txt", encoding='UTF8') as quotes:
+        data = quotes.readlines()
+        choice = random.choice(data)
+        return choice
+
+
+def send_email():
+    my_email = os.getenv("GMAIL_USER")
+    password = os.getenv("GMAIL_PASSWORD")
+    to_email = os.getenv("YAHOO_EMAIL")
+
+    quote = get_quote()
+    message = f"Subject:Quote of the day\n\n{quote}".encode("utf-8")
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=to_email,
+            msg=message)
+
+
+now = dt.datetime.now()
+
+if now.weekday() == 0:
+    send_email()
