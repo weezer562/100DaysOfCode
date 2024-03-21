450a5823720b8d496a930924cb0e076ff8da1e87
weezer562 <weezer562@gmail.com>
2024-03-20 22:11:29 -0700

    Day36 - Stock Trading Alerts

--- Day36-StockTradingAlerts/main.py
+++ Day36-StockTradingAlerts/main.py
@@ -1,24 +1,93 @@
-STOCK = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+from datetime import datetime, timedelta
+import itertools
+import json
+import os
+from twilio.rest import Client
 
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+import requests
 
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+STOCK = "AAPL"
+COMPANY_NAME = "Apple Inc"
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_SID = os.getenv("TWILIO_SID")
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+status = 0
 
 
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+def check_stock_diff():
+    global status
+    alpha_api_key = os.getenv("ALPHA_ADVANTAGE_API_KEY")
+    num_days = 2
 
+    parameters = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": STOCK,
+        "apikey": alpha_api_key,
+        "datatype": "json"
+    }
+
+    response = requests.get(url="https://www.alphavantage.co/query", params=parameters)
+    response.raise_for_status()
+
+    json_response = json.loads(response.text)
+    days_data = dict(itertools.islice(json_response["Time Series (Daily)"].items(), num_days))
+
+    today = datetime.now()
+    today_formatted = today.strftime("%Y-%m-%d")
+
+    yesterday = today - timedelta(days=1)
+    yesterday_formatted = yesterday.strftime("%Y-%m-%d")
+
+    today_close = days_data[today_formatted]["4. close"]
+    yesterday_close = days_data[yesterday_formatted]["4. close"]
+
+    diff = 1 - abs(today_close / yesterday_close)
+    if today_close > yesterday_close:
+        status = "🔺"
+    else:
+        status = "🔻"
+
+    return diff
+
+
+def get_news(dec_change):
+    news_api_key = os.getenv("NEWSAPI_API_KEY")
+
+    parameters = {
+        "q": f"{COMPANY_NAME}",
+        "pageSize": 3,
+        "page": 1,
+        "apikey": news_api_key
+    }
+
+    response = requests.get(url="https://newsapi.org/v2/everything", params=parameters)
+    response.raise_for_status()
+
+    json_response = json.loads(response.text)
+
+    message = ""
+    for index in range(3):
+        message += (f"{STOCK}: {status}{dec_change*100}%\n{json_response["articles"][index]["title"]}\n"
+                    f"Brief: {json_response["articles"][index]["description"]}")
+        message += "\n"
+
+    send_sms(message)
+
+
+def send_sms(message):
+    my_twilio_number = os.getenv("TWILIO_PHONE_NUMBER")
+    my_number = os.getenv("MY_NUMBER")
+
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+    message = client.messages \
+        .create(
+            body=message,
+            from_=my_twilio_number,
+            to=my_number
+        )
+    print(message.status)
+
+
+change = check_stock_diff()
+if change > .05:
+    get_news(change)
