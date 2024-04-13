64c1e896349ac203f1aa4cfedc1794ad5ac5aa16
weezer562 <weezer562@gmail.com>
2024-04-13 00:20:56 -0700

    Day55 - Higher Lower Site * practice for Day54  updates

--- Day54-HelloFlask/hello.py
+++ Day54-HelloFlask/hello.py
@@ -3,13 +3,44 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+
+    return wrapper
+
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+
+    return wrapper
+
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+
+    return wrapper
+
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
 def say_bye():
     return "<p>Bye<p>"
 
+
+@app.route('/username/<name>/<int:age>')
+def greet(name, age):
+    return f"Hello there, {name}, you are {age} old"
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
